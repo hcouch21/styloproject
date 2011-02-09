8204a8e98f8b416836c90c1d14cea68169710c98
Matt Tornetta <Matt Tornetta@localhost>
2011-02-09 17:15:07 -0500

    Fixing spacing and using Python instead of Python2

--- test-stylo.py
+++ test-stylo.py
@@ -5,8 +5,8 @@ import os
 from subprocess import *
 
 if __name__ == "__main__":
-  test_module_names = os.listdir("./tests/")
-  
-  for tmn in test_module_names:
+    test_module_names = os.listdir("./tests/")
+
+for tmn in test_module_names:
     if tmn.endswith(".py"):
-      Popen(["python2", "tests/%s" % tmn]).communicate()
+        Popen(["python", "tests/%s" % tmn]).communicate()
