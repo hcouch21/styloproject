afd0abef5e4c46abb44c8ff88d018ffdaddfbcd6
Matt Tornetta <Matt Tornetta@localhost>
2011-02-10 21:56:12 -0500

    Adding new tests for features only complete one is charcount right now

---
+++ tests/TestLinguisticFeatures.py
@@ -0,0 +1,42 @@
+import unittest
+
+import imp
+import os
+import sys
+
+class  LinguisticFeaturesTestCase(unittest.TestCase):
+    def setUp(self):
+        sys.path.append("./src")
+
+    #def tearDown(self):
+    #    self.foo.dispose()
+    #    self.foo = None
+
+    def get_linguistic_feature(self, name):
+        mod = imp.load_source(name, "src/features/%s.py" % name)
+        return getattr(mod, name)()
+
+    def test_char_count(self):
+        correct = [2870, 3001, 3052, 3182, 2906]
+        
+        lin_feat = self.get_linguistic_feature("CharCount")
+
+        count = 0
+        for file in os.listdir("./tests/data/"):
+            with open("tests/data/%s" % file, "r") as f:
+                val = lin_feat.extract(f.read())
+                
+                if val != correct[count]:
+                    self.fail("CharCount for %s calculated value %d is not equal to given value %d." % (file, val, correct[count]))
+
+            count += 1
+
+    def test_punct_count(self):
+        pass
+
+    def test_word_count(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
+
