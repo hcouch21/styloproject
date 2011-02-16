7d692adeac583d63d7234d1b300edd41c9f58c10
Matt Tornetta <Matt Tornetta@localhost>
2011-02-16 13:38:25 -0500

    Make names more consistent

--- src/stylo.py
+++ src/stylo.py
@@ -15,7 +15,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with Stylo.  If not, see <http://www.gnu.org/licenses/>.
 
-from PluginManager import *
+from PlugInManager import *
 from FeatureFactory import *
 
 import sys
