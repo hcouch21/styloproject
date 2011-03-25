dddb89b184899f114a531794f67aa9758de7b2a4
Matt Tornetta <Matt Tornetta@localhost>
2011-03-25 16:48:52 -0400

    Removed unused import of sys

--- src/plugins/FeatureExtractor/FeatureExtractor.py
+++ src/plugins/FeatureExtractor/FeatureExtractor.py
@@ -13,8 +13,6 @@
 #    You should have received a copy of the GNU General Public License
 #    along with Stylo.  If not, see <http://www.gnu.org/licenses/>.
 
-import sys
-
 from plugins.FeatureExtractor.FeatureFactory import *
 from PlugInInterface import *
 
